84847a6e1702b466ed2811bfc83c3a75bacd07c4
Arthurzano <159539643+Arthurzano@users.noreply.github.com>
2024-10-07 07:38:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Falas',
+    'Quem diz "eles querem mais de mim?tudo bem"',
+    'essa é uma fala do yoru'
+)
+
+criaCartao(
+    'Agentes',
+    'Qual agente tem poder de fogo?',
+    'Phoenix'
+)
+
+criaCartao(
+    'mapas',
+    'quais mapas tem 3 bombsites?',
+    'lotus e haven'
+)
+
+criaCartao(
+    'Armas',
+    'Qual arma da 150 de dano no peito?',
+    'Operator'
+)
